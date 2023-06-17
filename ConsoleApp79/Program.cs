e075b1701aca07b708562cd74a0efbe7733c5ca7
NHOY <NHOY@2210DR000919>
2023-06-17 16:27:18 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp79/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace ConsoleApp79
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            int number;
+            int numberStart = 0;
+            int firstDivisor = 3;
+            int secondDivider = 5;
+            int maxNumber = 100;
+            float finalResult = 0;
+            number = random.Next(maxNumber + 1);
+
+            Console.WriteLine("Ваше число:" + number);
+
+            for (int i = numberStart; i <= number; i++)
+            {
+                if (i % firstDivisor == 0 || i % secondDivider == 0)
+                {
+                    finalResult += i;
+                }
+            }
+
+            Console.WriteLine("Общая сумма - " + finalResult);
+        }
+    }
+}
